2a054a03b4c9be721cef2fc8d9b1d4aad784979d
Fuzail-Raza <samreen1554@gmail.com>
2026-06-27 21:38:00 +0500

    second commit 2

--- server.js
+++ server.js
@@ -19,20 +19,23 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend Running..." });
 });
 
-// MongoDB Connection Function
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/lumora"
-    );
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.log("DB Error:", err);
-  }
-};
 
-// Connect DB once
-connectDB();
+let isConnected = false;
+
+async function connectToMongoDB() {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToMongoDB();
 
 
 
